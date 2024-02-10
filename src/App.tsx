50d50230ca4c43a5582dfd42a5299075f2430259
Devdeep Ghosh <devarmforce01@gmail.com>
2024-02-10 06:10:27 +0530

    shadcn installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-const App = () => {
-  return <div>App</div>;
-};
+import { ThemeProvider } from "./components/theme-provider";
+
+function App({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      {children}
+    </ThemeProvider>
+  );
+}
 
 export default App;
