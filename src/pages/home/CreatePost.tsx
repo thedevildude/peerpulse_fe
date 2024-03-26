7c6e4af6f01563614199300e6c8d64cc49d43022
Devdeep Ghosh <devarmforce01@gmail.com>
2024-03-27 03:11:29 +0530

    Post and poll creation added to Dialog Modal

--- src/pages/home/CreatePost.tsx
+++ src/pages/home/CreatePost.tsx
@@ -2,28 +2,42 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { ImageIcon, VoteIcon } from "lucide-react";
 import PostForm from "./PostForm";
 import PollForm from "./PollForm";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Avatar } from "@/components/ui/avatar";
+import { AvatarImage } from "@radix-ui/react-avatar";
 
 export const CreatePost = () => {
   return (
-    <div className="flex w-full flex-col gap-5 rounded-md bg-white">
-      <Tabs defaultValue="post" className="p-5">
-        <TabsList>
-          <TabsTrigger value="post">
-            <ImageIcon size={20} />
-            <p className="ml-2">Post</p>
-          </TabsTrigger>
-          <TabsTrigger value="poll">
-            <VoteIcon size={20} />
-            <p className="ml-2">Poll</p>
-          </TabsTrigger>
-        </TabsList>
-        <TabsContent value="post">
-          <PostForm />
-        </TabsContent>
-        <TabsContent value="poll">
-          <PollForm />
-        </TabsContent>
-      </Tabs>
-    </div>
+    <Dialog>
+      <DialogTrigger className="flex w-full cursor-pointer items-center gap-3 rounded-md bg-white p-5 transition-colors duration-200 ease-in-out hover:bg-gray-100 dark:bg-black">
+        <Avatar>
+          <AvatarImage src="https://avatars.dicebear.com/api/avataaars/john-doe.svg" />
+        </Avatar>
+        <Input className="disabled" placeholder="What's on your mind?" />
+      </DialogTrigger>
+      <DialogContent>
+        <div className="flex w-full flex-col gap-5 rounded-md">
+          <Tabs defaultValue="post" className="p-5">
+            <TabsList>
+              <TabsTrigger value="post">
+                <ImageIcon size={20} />
+                <p className="ml-2">Post</p>
+              </TabsTrigger>
+              <TabsTrigger value="poll">
+                <VoteIcon size={20} />
+                <p className="ml-2">Poll</p>
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="post">
+              <PostForm />
+            </TabsContent>
+            <TabsContent value="poll">
+              <PollForm />
+            </TabsContent>
+          </Tabs>
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 };
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,22 +1,17 @@
 import { useAuthProvider } from "@/providers/authProvider";
-import { useTranslation } from "react-i18next";
 import UnverifiedEmailAlert from "./UnverifiedEmailAlert";
 import { CreatePost } from "./CreatePost";
 
 const Home = () => {
-  const { t } = useTranslation();
   const { user } = useAuthProvider();
   return (
     <div>
-      {t("homepage")}
-      <p>{t("today-date", { date: new Date() })}</p>
-      <p>{t("right-now", { time: new Date() })}</p>
       {user && !user.isEmailVerified && <UnverifiedEmailAlert />}
       <div className="grid h-screen grid-cols-2 md:grid-cols-4">
         <div className="col-span-1 hidden bg-gray-200 md:block">
           Left Sidebar
         </div>
-        <div className="col-span-2 px-5">
+        <div className="col-span-2">
           <CreatePost />
         </div>
         <div className="col-span-1 hidden bg-gray-200 md:block">
